6ef544ce6ba1834df5d450a387ac5c324db54a3e
RutujaaJagtap <88143602+RutujaaJagtap@users.noreply.github.com>
2021-07-29 19:45:54 +0530

    Add files via upload
    assignment2 completed

---
+++ assign2.py
@@ -0,0 +1,11 @@
+print(hii)
+assignlist = [21, 5, 8, 52, 21, 87]
+r_item = 21
+print(original list:assignlist)
+
+ts occurrences
+for item in assignlist:
+	if(item==r_item):
+		assignlist.remove(r_item)
+print(updated list)
+print(assignlist)
